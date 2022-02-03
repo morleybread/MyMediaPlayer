e281c361f0a99043c81b1904bf80c1aaf12a4eae
morleybread <morleybread@gamil.com>
2022-02-03 20:41:03 +0800

    Create video1

---
+++ src/main/java/com/morley/myvideoplayer/FileArrayCreater.java
@@ -0,0 +1,36 @@
+package com.morley.myvideoplayer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import javafx.collections.ObservableList;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class FileArrayCreater {
+
+    public ArrayList<File> createFileObjectArray(){
+        ObservableList<MediaPlayer> observableList = null;
+        ArrayList<File> files=new ArrayList<>();
+        for(File m: Objects.requireNonNull(new File("video_test").listFiles())){
+//            System.out.println(m);//打印一级目录文件对象
+            for (File f: Objects.requireNonNull(m.listFiles())){
+                if(f.isFile()&&f.getName().lastIndexOf(4)==-1){  //判断是否为文件 并且为mp4结尾
+                    files.add(f);
+                }
+            }
+        }
+        return files;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        FileArrayCreater fileArrayCreater =new FileArrayCreater();
+        System.out.println(fileArrayCreater.createFileObjectArray());
+
+    }
+
+}
--- src/main/java/com/morley/myvideoplayer/HelloApplication.java
+++ src/main/java/com/morley/myvideoplayer/HelloApplication.java
@@ -21,68 +21,69 @@ import java.util.Arrays;
 
 public class HelloApplication extends Application {
      private   MediaView mediaView;
-     private   ObservableList<MediaPlayer> observableList; //needed
-
-   public HelloApplication(ObservableList<MediaPlayer> observableList){
-       this.observableList=observableList;
-   }
 
     @Override
     public void start(Stage stage) throws IOException {
-        File file=new File("video_test/Best air shows in the world.mp4");
-        Media media=new Media(file.toURI().toString());
-   MediaPlayer mediaPlayer=new MediaPlayer(media);
-this.mediaView=new MediaView(mediaPlayer);
-
-//        Button button=new Button("<<");
-//        Button button1=new Button(">>");
-        Button nextButton=new Button("next");
-        Button pauseButton=new Button("pause");
-
-        BorderPane borderPane=new BorderPane();
-
-        HBox hBox=new HBox(10);
-        hBox.setAlignment(Pos.CENTER);
-        hBox.getChildren().addAll(nextButton,pauseButton);
-        borderPane.setCenter(this.mediaView);
-        borderPane.setBottom(hBox);
-
-
-        nextButton.setOnAction(e->{
-          mediaPlayer.seek(Duration.INDEFINITE);
-        });
+        FileArrayCreater fileArrayCreater=new FileArrayCreater();
+        System.out.println(fileArrayCreater.createFileObjectArray());
 
 
 
-        Scene scene=new Scene(borderPane,650,500);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
 
-mediaPlayer.setAutoPlay(true);
-mediaPlayer.setOnEndOfMedia(new Runnable() {
-    @Override
-    public void run() {
 
-                    File file1=new File("video_test/The Course.mp4");
-            Media media1=new Media(file1.toURI().toString());
-            MediaPlayer mediaPlayer1=new MediaPlayer(media1);
-           mediaView.setMediaPlayer(mediaPlayer1);
-           mediaPlayer1.setAutoPlay(true);
 
-    }
-});
+
+
+//        Media media=new Media(file.toURI().toString());
+//   MediaPlayer mediaPlayer=new MediaPlayer(media);
+//this.mediaView=new MediaView(mediaPlayer);
+//
+////        Button button=new Button("<<");
+////        Button button1=new Button(">>");
+//        Button nextButton=new Button("next");
+//        Button pauseButton=new Button("pause");
+//
+//        BorderPane borderPane=new BorderPane();
+//
+//        HBox hBox=new HBox(10);
+//        hBox.setAlignment(Pos.CENTER);
+//        hBox.getChildren().addAll(nextButton,pauseButton);
+//        borderPane.setCenter(this.mediaView);
+//        borderPane.setBottom(hBox);
+//
+//
+//        nextButton.setOnAction(e->{
+//          mediaPlayer.seek(Duration.INDEFINITE);
+//        });
+//
+//
+//
+//        Scene scene=new Scene(borderPane,650,500);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//
+//mediaPlayer.setAutoPlay(true);
+//mediaPlayer.setOnEndOfMedia(new Runnable() {
+//    @Override
+//    public void run() {
+//
+//                    File file1=new File("video_test/The Course.mp4");
+//            Media media1=new Media(file1.toURI().toString());
+//            MediaPlayer mediaPlayer1=new MediaPlayer(media1);
+//           mediaView.setMediaPlayer(mediaPlayer1);
+//           mediaPlayer1.setAutoPlay(true);
+//
+//    }
+//});
+
     }
 
 
 
 
     public static void main(String[] args) {
+launch(args);
 
-//         launch(args);
-        File file=new File("video_test");
-        System.out.println(Arrays.toString(file.list()));
-
-//        Application.launch(args);
     }
 }
